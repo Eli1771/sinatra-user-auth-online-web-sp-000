80abae255ddbfd1376091e08ac2098067c89a603
elijahmm17 <elijahmm17@gmail.com>
2019-07-19 22:12:52 +0000

    Automatically backed up by Learn

--- db/migrate/20190719221032_create_users.rb
+++ db/migrate/20190719221032_create_users.rb
@@ -1,7 +1,9 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
-      t.column :string :name
+      t.column :string, :name 
+      t.column :string, :email
+      t.column :string, :password
       
     end 
   end
